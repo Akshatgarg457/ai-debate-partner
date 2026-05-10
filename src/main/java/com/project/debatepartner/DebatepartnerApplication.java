a324ba7e17032f958c32efdb528ee31557eff6c7
Akshat Garg <akshatgarg120@gmail.com>
2026-05-10 23:00:35 +0530

    final permanent railway fix

--- src/main/java/com/project/debatepartner/DebatepartnerApplication.java
+++ src/main/java/com/project/debatepartner/DebatepartnerApplication.java
@@ -2,9 +2,12 @@ package com.project.debatepartner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.project.debatepartner.repository")
+@EntityScan(basePackages = "com.project.debatepartner.model")
 public class DebatepartnerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/project/debatepartner/repository/UserRepository.java
+++ src/main/java/com/project/debatepartner/repository/UserRepository.java
@@ -2,11 +2,8 @@ package com.project.debatepartner.repository;
 
 import com.project.debatepartner.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByUsername(String username);
