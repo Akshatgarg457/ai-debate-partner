2752b1d0c855c02ff47b40995bb8d8b9678544f1
Akshat Garg <akshatgarg120@gmail.com>
2026-05-10 22:48:14 +0530

    fix user repository

--- src/main/java/com/project/debatepartner/repository/UserRepository.java
+++ src/main/java/com/project/debatepartner/repository/UserRepository.java
@@ -2,11 +2,12 @@ package com.project.debatepartner.repository;
 
 import com.project.debatepartner.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository
-        extends JpaRepository<User, Integer> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByUsername(String username);
 
