1f9cc9a8e3d3c1c3a586fa7d20a74d909e582c71
Akshat Garg <akshatgarg120@gmail.com>
2026-05-18 00:57:50 +0530

    fixed debate room result

--- src/main/java/com/project/debatepartner/service/DebateRoomService.java
+++ src/main/java/com/project/debatepartner/service/DebateRoomService.java
@@ -1,70 +1,121 @@
-package com.project.debatepartner.service;
+package com.project.debatepartner.model;
 
-import com.project.debatepartner.model.DebateRoom;
-import com.project.debatepartner.model.Message;
-import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
-import java.util.*;
+public class DebateRoom {
 
-@Service
-public class DebateRoomService {
+    private String roomId;
 
-    private final Map<String, DebateRoom> rooms = new HashMap<>();
+    private String topic;
 
-    // CREATE ROOM
-    public DebateRoom createRoom(String topic, String username) {
+    private String player1;
 
-        DebateRoom room = new DebateRoom(topic, username);
+    private String player2;
 
-        rooms.put(room.getRoomId(), room);
+    private boolean started;
 
-        return room;
+    private List<Message> messages =
+            new ArrayList<>();
+
+
+    // ADD THESE
+
+    private String result;
+
+    private String winner;
+
+
+
+    public DebateRoom(
+            String topic,
+            String username
+    ){
+
+        this.roomId=
+                UUID.randomUUID()
+                .toString();
+
+        this.topic=
+                topic;
+
+        this.player1=
+                username;
+
+        this.started=
+                false;
     }
 
-    // JOIN ROOM
-    public DebateRoom joinRoom(String roomId, String username) {
 
-        DebateRoom room = rooms.get(roomId);
+    public void addMessage(
+            Message msg
+    ){
 
-        if (room == null) {
-            return null;
-        }
-
-        if (room.getPlayer2() != null) {
-            return null;
-        }
-
-        room.setPlayer2(username);
-        room.setStarted(true);
-
-        return room;
+        messages.add(msg);
     }
 
-    // SEND MESSAGE
-    public void sendMessage(String roomId, Message message) {
 
-        DebateRoom room = rooms.get(roomId);
 
-        if (room != null) {
-            room.addMessage(message);
-        }
+    // GETTERS / SETTERS
+
+    public String getRoomId() {
+        return roomId;
     }
 
-    // GET MESSAGES
-    public List<Message> getMessages(String roomId) {
-
-        DebateRoom room = rooms.get(roomId);
-
-        if (room == null) {
-            return new ArrayList<>();
-        }
-
-        return room.getMessages();
+    public String getTopic() {
+        return topic;
     }
 
-    // GET ROOM
-    public DebateRoom getRoom(String roomId) {
-
-        return rooms.get(roomId);
+    public String getPlayer1() {
+        return player1;
     }
+
+    public String getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(
+            String player2
+    ){
+        this.player2=player2;
+    }
+
+    public boolean isStarted() {
+        return started;
+    }
+
+    public void setStarted(
+            boolean started
+    ){
+        this.started=started;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+
+    // RESULT
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(
+            String result
+    ){
+        this.result=result;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(
+            String winner
+    ){
+        this.winner=winner;
+    }
+
 }
